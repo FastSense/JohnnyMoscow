37e84d9544deee142cbbac8db5b992759b88936a
Your Name <you@example.com>
2021-05-04 12:25:26 +0300

    joy_head: reverse roll axis of the helmet

--- onpult/joy_head.py
+++ onpult/joy_head.py
@@ -5,7 +5,7 @@ import socket
 import time
 from src import joystick, glass
 
-serverIp = 'localhost'  # необходимо указать ip робота
+serverIp = '192.168.118.125'  # необходимо указать ip робота
 serverPort = 9009
 sendFreq = 0.05    # слать 5 пакетов в сек
 _exit = False   # переменная выхода из потоков
@@ -24,12 +24,13 @@ J.start()
 J.info()
 
 try:
+    i = 0
     while True:
         try:
             angle = glasses.angleHead
-            head_package["yaw"] = angle[0]
-            head_package["pitch"] = angle[1]
-            head_package["roll"] = angle[2]
+            head_package["yaw"] = round(angle[0], 3)
+            head_package["pitch"] = round(angle[1], 3)
+            head_package["roll"] = -round(angle[2], 3)
             sock.sendto(json.dumps(head_package, ensure_ascii=False).encode("utf8"), (serverIp, serverPort))  # кодируем
             # json и отправляем его
         except BrokenPipeError:
@@ -38,14 +39,14 @@ try:
             print("Ошибка при отправке пакета с данными углов головы робота: " + e.__repr__())
 
         try:
-            joy_package["x"] = -J.axis['x']  # могут быть другие оси, можно переназначить, но необходимо выяснить их название
-            joy_package["y"] = -J.axis['y']
+            joy_package["x"] = -round(J.axis['x'], 3)  # могут быть другие оси, можно переназначить, но необходимо выяснить их название
+            joy_package["y"] = -round(J.axis['y'], 3)
             sock.sendto(json.dumps(joy_package, ensure_ascii=False).encode("utf8"), (serverIp, serverPort))  # кодируем json и
         except BrokenPipeError:
             print("Ошибка при отправке пакета с данными движения робота")
         except Exception as e:
             print("Ошибка при отправке пакета с данными движения робота: " + e.__repr__())
-    time.sleep(0.05)
+        time.sleep(0.08)
 except KeyboardInterrupt:
     J.exit()
     sock.close()
