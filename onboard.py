faac0dd56a74d00d967d2ca50433b8b7cf2307e6
Your Name <you@example.com>
2021-06-18 11:29:05 +0100

    onboard: adding exponential correction for wheels inputs

--- onboard.py
+++ onboard.py
@@ -9,6 +9,7 @@ from rise.board.robothandle import JohnyHandle
 import json
 import logging
 import redis
+import math
 
 redis_connection = redis.Redis(host='192.168.118.96', port=6379, db=0, password='DTL@b2021')
 pubsub = redis_connection.pubsub()
@@ -87,6 +88,7 @@ logging.info("start receive packages")
 while True:
     msg = pubsub.get_message()
 
+    #time.sleep(0.1)
     if msg is not None:
         if msg['data'] != 1:
             data = msg['data']
@@ -99,19 +101,37 @@ while True:
         package = json.loads(data.decode('utf-8'))
         if package["command"] == "WHEELS":
             x, y = package["data"]["x"], package["data"]["y"]
-            x = -min(max(-1.0, x), 1.0)
+            x = min(max(-1.0, x), 1.0)
             y = -min(max(-1.0, y), 1.0)
-            # johnyHandler.vector(x, y)
+
+            # Exponential scaling for input
+            kl = 1
+            ka = 1
+            if y < 0:
+                y = abs(y)
+                kl = -1
+            if x < 0:
+                x = abs(x)
+                ka = -1
+
+            # Exponential scaling for inputs
+            y = kl * (math.exp(y) - 1) / (math.e - 1)
+            x = ka * (math.exp(x) - 1) / (math.e - 1)
+
+            johnyHandler.vector(x, y)
             timer = time.time()
 
         if package["command"] == "HEAD":
             yaw, pitch, roll = package["data"]["rot"]["y"], package["data"]["rot"]["p"], package["data"]["rot"]["r"]
-            pitch = pitch * 1.8
-            yaw, pitch, roll = int(yaw), int(pitch), int(roll)
+            yaw, pitch, roll = int(yaw), int(pitch), int(-roll)
             if yaw > 180:
                 yaw = -1 * (360 - yaw)
             if pitch > 180:
                 pitch = -1 * (360 - pitch)
+            if roll > 180:
+                roll = -1 * (360 - roll)
+
+            pitch = int(pitch * 1.8)
 
             if pitch > 130:
                 pitch = 130
@@ -123,7 +143,10 @@ while True:
             if yaw < - 110:
                 yaw = -110
 
-            johnyHandler.setHeadPosition(-yaw, pitch, roll)
+            johnyHandler.setHeadPosition(-yaw, pitch, -roll)
+
+            print(-yaw, pitch, roll)
 
     except Exception as e:
         logging.error("drop package: {data} from {addr}: {err}".format(data=data, addr=addr, err=e.__str__()))
+
